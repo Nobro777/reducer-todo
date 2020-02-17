a0efa905dc7f9e4fd4d1e75d889c473edce5f907
Nobro777 <52586100+Nobro777@users.noreply.github.com>
2020-02-17 15:59:28 -0800

    mvp done

---
+++ reducer-todo/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Nav from './components/Nav';
+import Todo from './components/Todo';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
